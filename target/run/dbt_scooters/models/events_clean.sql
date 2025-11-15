2e0f2f495a03c99b3e512dfcf502f9db8ede5416
Vitaly_Flerin <flerinvs@m2.ru>
2025-11-15 19:59:35 +0300

    добавил модель

--- target/run/dbt_scooters/models/events_clean.sql
+++ target/run/dbt_scooters/models/events_clean.sql
@@ -3,7 +3,7 @@
         delete from "dev_m0z9"."dbt"."events_clean" as DBT_INTERNAL_DEST
         where (user_id, timestamp, type_id) in (
             select distinct user_id, timestamp, type_id
-            from "events_clean__dbt_tmp114933896845" as DBT_INTERNAL_SOURCE
+            from "events_clean__dbt_tmp192454089685" as DBT_INTERNAL_SOURCE
         );
 
     
@@ -11,6 +11,6 @@
     insert into "dev_m0z9"."dbt"."events_clean" ("user_id", "timestamp", "type_id", "updated_at")
     (
         select "user_id", "timestamp", "type_id", "updated_at"
-        from "events_clean__dbt_tmp114933896845"
+        from "events_clean__dbt_tmp192454089685"
     )
   
